e48a8cd2ef2aebde1c9b92e412b7ae8d02ebee1b
flamrdevs <flamrdevs@gmail.com>
2023-09-16 12:05:01 +0700

    chore: improve typing and docs

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,54 @@ import type { Region } from "@regions-of-indonesia/types";
 
 const REGION_CODE_REGEXP = new RegExp(/^\d+(\.\d+)*$/);
 
-const isTypeofObject = (value?: unknown): value is object | null => typeof value === "object";
-const isTypeofString = (value?: unknown): value is string => typeof value === "string";
+/**
+ *
+ * @param value string
+ * @returns valid region code
+ */
+const isRegionCode = (value?: unknown): value is string => typeof value === "string" && REGION_CODE_REGEXP.test(value);
 
-const isRegionCode = (value?: unknown): value is string => isTypeofString(value) && REGION_CODE_REGEXP.test(value);
-const isRegionName = (value?: unknown): value is string => isTypeofString(value);
+/**
+ *
+ * @param value string
+ * @returns valid region name
+ */
+const isRegionName = (value?: unknown): value is string => typeof value === "string";
 
+/**
+ *
+ * @param value region
+ * @returns valid region
+ */
 const isRegion = (value?: unknown): value is Region =>
-  isTypeofObject(value) && value != null && "code" in value && "name" in value && isRegionCode(value.code) && isRegionName(value.name);
+  typeof value === "object" && value != null && "code" in value && "name" in value && isRegionCode(value.code) && isRegionName(value.name);
+
+/**
+ *
+ * @param value regions
+ * @returns valid regions
+ */
 const isRegions = (value?: unknown): value is Region[] => Array.isArray(value) && value.every(isRegion);
 
+/**
+ *
+ * @param values splitted code
+ * @returns joined code
+ *
+ * @example
+ * joinRegionCode(["11", "01", "01", "2001"]); // "11.01.01.2001"
+ */
 const joinRegionCode = (values: string[]) => values.join(".");
+
+/**
+ *
+ * @param value joined code
+ * @returns splitted code
+ *
+ * @example
+ * splitRegionCode("11.01.01.2001"); // ["11", "01", "01", "2001"]
+ */
 const splitRegionCode = (value: string) => value.split(".");
 
 export { REGION_CODE_REGEXP };
-export { isTypeofObject, isTypeofString };
 export { isRegionCode, isRegionName, isRegion, isRegions, joinRegionCode, splitRegionCode };
